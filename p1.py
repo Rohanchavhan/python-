7e2dd91a43a6598a89bcfb9f0096514feeec272e
Rohanchavhan <rohanchavhan669@gmail.com>
2024-12-18 16:21:10 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,20 @@
+'''
+num1=54
+num2=45
+add=num1+num2
+print("the addition is",add)
+'''
+
+#add two number using user input
+a=int(input("enter firsh num"))
+b=int(intut("enter second num"))
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+rem=a%b
+print("the sum is",add)
+print("the sum is",sub)
+print("the sum is",mul)
+print("the sum is",div)
+print("the sum is",rem)
---
+++ p2.py
@@ -0,0 +1,15 @@
+#calculate aera of circle
+redius=int(input("enter readius of a circle"))
+area=3.14*redius*redius
+print("the area of circle is",area)
+
+# Input three numbers
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+
+# Calculate average
+average = (num1 + num2 + num3) / 3
+
+# Print average
+print("Average:", average)
